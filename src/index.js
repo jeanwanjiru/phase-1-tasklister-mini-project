45a34b48c875aedabb0d4a1ecdcb9cc90f1c0017
jeanwanjiru <jeanwanjiru06@gmail.com>
2023-04-04 13:37:51 -0400

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ document.addEventListener("DOMContentLoaded", () => { 
+ document.querySelector('form').addEventListener('submit',(e) => {
+e.preventDefault()
+handleToDo(e.target.new_todo.value)
+ })
 });
+function handleToDo(todo){
+    let p =document.createElement('p')
+    let btn = document.createElement('button')
+}
